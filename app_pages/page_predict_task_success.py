96fad97b270def0275b7312028d161cb1efd3d4e
Markus-Hefner <tstrahldrucker@gmail.com>
2025-06-15 20:16:41 +0000

    Create content for 'page_predict_taks_success.py' file

---
+++ app_pages/page_predict_task_success.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.data_management import load_ai_developer_data, load_pkl_file
+from src.machine_learning.evaluate_clf import clf_performance
+
+# copied and adjusted
+def page_predict_task_success_body():
+
+    version = 'v1'
+    # load needed files
+    task_success_pipe_dc_fe = load_pkl_file(
+        f'outputs/ml_pipeline/predict_task_success/{version}/clf_pipeline_data_cleaning_feat_eng.pkl')
+    task_success_pipe_model = load_pkl_file(
+        f"outputs/ml_pipeline/predict_task_success/{version}/clf_pipeline_model.pkl")
+    task_success_feat_importance = plt.imread(
+        f"outputs/ml_pipeline/predict_task_success/{version}/features_importance.png")
+    X_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_task_success/{version}/X_train.csv")
+    X_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_task_success/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_task_success/{version}/y_train.csv").values
+    y_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_task_success/{version}/y_test.csv").values
+
+    st.write("### ML Pipeline: Predict Task Success")
+    # display pipeline training summary conclusions
+    st.info(
+        f"* The pipeline was tuned aiming at least 0.80 Recall on 'Task Success' class, "
+        f"since we are interested in this project in detecting a potential developer "
+        f"who might need some help. \n"
+        f"* The pipeline performance on train and test set is 0.90 and 0.95, respectively."
+    )
+
+    # show pipelines
+    st.write("---")
+    st.write("#### There were 2 ML Pipelines created.")
+
+    st.write(" * The first is responsible for data cleaning and feature engineering.")
+    st.write(task_success_pipe_dc_fe)
+
+    st.write("* The second is for feature scaling and modelling.")
+    st.write(task_success_pipe_model)
+
+    st.info(
+        f"* However, since there was no missing data in the dataset and "
+        f"`ai_usage_hours` was the only variable that improved by feature "
+        f"engineering but was not one of the two most important factors that "
+        f"were eventually used to train the model, the pipeline for data "
+        f"cleaning and feature engineering was not applied."
+    )
+
+    # show feature importance plot
+    st.write("---")
+    st.write("* The features the model was trained and their importance.")
+    st.write(X_train.columns.to_list())
+    st.image(task_success_feat_importance)
+
+    # We don't need to apply dc_fe pipeline, since X_train and X_test
+    # were already transformed in the jupyter notebook (Predict Task Success.ipynb)
+
+    # evaluate performance on train and test set
+    st.write("---")
+    st.write("### Pipeline Performance")
+    clf_performance(X_train=X_train, y_train=y_train,
+                    X_test=X_test, y_test=y_test,
+                    pipeline=task_success_pipe_model,
+                    label_map=["No Task Success", "Task Success"])
---
+++ src/machine_learning/predict_task_success.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# copied and adapted changed all churn to task_success and statement
+def predict_task_success(X_live, task_success_features, task_success_pipeline_dc_fe, task_success_pipeline_model):
+
+    # from live data, subset features related to this pipeline
+    X_live_task_success = X_live.filter(task_success_features)
+
+    # apply data cleaning / feat engine pipeline to live data
+    X_live_task_success_dc_fe = task_success_pipeline_dc_fe.transform(X_live_task_success)
+
+    # predict
+    task_success_prediction = task_success_pipeline_model.predict(X_live_task_success_dc_fe)
+    task_success_prediction_proba = task_success_pipeline_model.predict_proba(
+        X_live_task_success_dc_fe)
+    # st.write(task_success_prediction_proba)
+
+    # Create a logic to display the results
+    task_success_prob = task_success_prediction_proba[0, task_success_prediction][0]*100
+    if task_success_prediction == 1:
+        task_success_result = 'will'
+    else:
+        task_success_result = 'will not'
+
+    statement = (
+        f'### There is {task_success_prob.round(1)}% probability '
+        f'that this developer **{task_success_result} have task success**.')
+
+    st.write(statement)
+
+    return task_success_prediction
