2afd5877411c9d38ff4abd5b6c1a25a9b057d93c
Markus-Hefner <tstrahldrucker@gmail.com>
2025-06-15 09:35:33 +0000

    Fix bug (see previous commits and commit message body
    CRTL-click on the path in the CLI and changed 'collection' to 'collection.abc'

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
-from app_pages.page_productivity_study import page_productivity_study_body
-from app_pages.page_ai_usage_study import page_ai_usage_study_body
-from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_predict_task_success import page_predict_task_success_body
+# from app_pages.page_productivity_study import page_productivity_study_body
+# from app_pages.page_ai_usage_study import page_ai_usage_study_body
+# from app_pages.page_project_hypothesis import page_project_hypothesis_body
+# from app_pages.page_predict_task_success import page_predict_task_success_body
 
 app = MultiPage(app_name= "PP5") # Create an instance of the app 
 
 # Add your app pages here using .add_page()
 app.add_page("Short Project Summary", page_summary_body)
-app.add_page("Productivity Study", page_productivity_study_body)
-app.add_page("AI Usage Study", page_ai_usage_study_body)
-app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-app.add_page("ML: Task Success", page_predict_task_success_body)
+# app.add_page("Productivity Study", page_productivity_study_body)
+# app.add_page("AI Usage Study", page_ai_usage_study_body)
+# app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+# app.add_page("ML: Task Success", page_predict_task_success_body)
 app.run() # Run the  app
