0b55c8dacde7b6bd3705182628a511a879a33386
Markus-Hefner <tstrahldrucker@gmail.com>
2025-06-15 19:37:38 +0000

    Edit 'page_project_hypothesis.py' file

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -34,6 +34,7 @@ def page_project_hypothesis_body():
         f"be justifed to see if it really is just the caffeine intake that "
         f"drives the successful completion of a task or if it just a proxy for "
         f"an underlying correlation that is the real reason. (E.g. maybe "
-        f"developers who take in more caffeine also code more hours like others "
-        f"but are also more alert thus making more progress with less errors.)"
+        f"developers who take in more caffeine also code the optimum amount of "
+        f"hours like others ut are also more alert thus making more progress "
+        f"with less errors.)"
     )
